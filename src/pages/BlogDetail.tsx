9754fdffc57c078e92d01b941898b18e788eeb38
AnkitKachhawa1 <117518485+AnkitKachhawa1@users.noreply.github.com>
2025-06-08 20:02:46 +0530

    Update BlogDetail.tsx

--- src/pages/BlogDetail.tsx
+++ src/pages/BlogDetail.tsx
@@ -1,1 +1,19 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { blogs } from "../data/blogs";
 
+const BlogDetail = () => {
+  const { id } = useParams();
+  const blog = blogs.find((b) => b.id === Number(id));
+
+  if (!blog) return <div>Blog not found</div>;
+
+  return (
+    <div>
+      <h2>{blog.title}</h2>
+      <p>{blog.content}</p>
+    </div>
+  );
+};
+
+export default BlogDetail;
