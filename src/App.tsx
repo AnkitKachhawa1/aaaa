57f28fcc1a7a1f3485abb1b81bd469785bca0bbd
AnkitKachhawa1 <117518485+AnkitKachhawa1@users.noreply.github.com>
2025-06-08 20:02:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import Layout from './components/Layout';
 import MutualFundPage from './pages/MutualFundPage';
 import GSTPage from './pages/GSTPage';
 import UpdatesPage from './pages/UpdatesPage';
+import BlogDetail from './pages/BlogDetail'; // NEW
+import NewBlog from './pages/NewBlog'; // NEW
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Navigate to="/mutualfund\" replace />} />
+        <Route index element={<Navigate to="/mutualfund" replace />} />
         <Route path="mutualfund" element={<MutualFundPage />} />
         <Route path="gst" element={<GSTPage />} />
         <Route path="updates" element={<UpdatesPage />} />
-        <Route path="*" element={<Navigate to="/mutualfund\" replace />} />
+        <Route path="blogs/new" element={<NewBlog />} /> {/* NEW */}
+        <Route path="blogs/:id" element={<BlogDetail />} /> {/* NEW */}
+        <Route path="*" element={<Navigate to="/mutualfund" replace />} />
       </Route>
     </Routes>
   );
