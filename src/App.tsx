6c97f15c7a224e6e44ac056a955407fe101d514e
AnkitKachhawa1 <117518485+AnkitKachhawa1@users.noreply.github.com>
2025-06-08 21:22:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import MutualFundPage from './pages/MutualFundPage';
 import GSTPage from './pages/GSTPage';
 import UpdatesPage from './pages/UpdatesPage';
-import BlogDetail from './pages/BlogDetail'; // NEW
-import NewBlog from './pages/NewBlog'; // NEW
-import { HelmetProvider } from 'react-helmet-async'; // ADD THIS LINE
+import BlogDetail from './pages/BlogDetail';
+import NewBlog from './pages/NewBlog';
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <HelmetProvider> {/* ADD THIS LINE */}
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="/mutualfund" replace />} />
-          <Route path="mutualfund" element={<MutualFundPage />} />
-          <Route path="gst" element={<GSTPage />} />
-          <Route path="updates" element={<UpdatesPage />} />
-          <Route path="blogs/new" element={<NewBlog />} /> {/* NEW */}
-          <Route path="blogs/:id" element={<BlogDetail />} /> {/* NEW */}
-          <Route path="*" element={<Navigate to="/mutualfund" replace />} />
-        </Route>
-      </Routes>
-    </HelmetProvider> {/* ADD THIS LINE */}
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Navigate to="/mutualfund" replace />} />
+            <Route path="mutualfund" element={<MutualFundPage />} />
+            <Route path="gst" element={<GSTPage />} />
+            <Route path="updates" element={<UpdatesPage />} />
+            <Route path="blogs/new" element={<NewBlog />} />
+            <Route path="blogs/:id" element={<BlogRoutes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
