1d802de4bd4428ec179ea9d70b5b1e6d6982ee65
AnkitKachhawa1 <117518485+AnkitKachhawa1@users.noreply.github.com>
2025-06-08 21:20:28 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,23 @@ import GSTPage from './pages/GSTPage';
 import UpdatesPage from './pages/UpdatesPage';
 import BlogDetail from './pages/BlogDetail'; // NEW
 import NewBlog from './pages/NewBlog'; // NEW
+import { HelmetProvider } from 'react-helmet-async'; // ADD THIS LINE
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Navigate to="/mutualfund" replace />} />
-        <Route path="mutualfund" element={<MutualFundPage />} />
-        <Route path="gst" element={<GSTPage />} />
-        <Route path="updates" element={<UpdatesPage />} />
-        <Route path="blogs/new" element={<NewBlog />} /> {/* NEW */}
-        <Route path="blogs/:id" element={<BlogDetail />} /> {/* NEW */}
-        <Route path="*" element={<Navigate to="/mutualfund" replace />} />
-      </Route>
-    </Routes>
+    <HelmetProvider> {/* ADD THIS LINE */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/mutualfund" replace />} />
+          <Route path="mutualfund" element={<MutualFundPage />} />
+          <Route path="gst" element={<GSTPage />} />
+          <Route path="updates" element={<UpdatesPage />} />
+          <Route path="blogs/new" element={<NewBlog />} /> {/* NEW */}
+          <Route path="blogs/:id" element={<BlogDetail />} /> {/* NEW */}
+          <Route path="*" element={<Navigate to="/mutualfund" replace />} />
+        </Route>
+      </Routes>
+    </HelmetProvider> {/* ADD THIS LINE */}
   );
 }
 
