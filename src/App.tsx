a63befeb7bc52b71f12dc9c1c8f0b233eb6fb30c
AnkitKachhawa1 <117518485+AnkitKachhawa1@users.noreply.github.com>
2025-06-08 21:28:55 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import MutualFundPage from './pages/MutualFundPage';
 import GSTPage from './pages/GSTPage';
 import UpdatesPage from './pages/UpdatesPage';
-import BlogDetail from './pages/BlogDetail';
-import NewBlog from './pages/NewBlog';
-import { HelmetProvider } from 'react-helmet-async';
+import BlogDetail from './pages/BlogDetail'; // NEW
+import NewBlog from './pages/NewBlog'; // NEW
 
 function App() {
   return (
-    <HelmetProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Navigate to="/mutualfund" replace />} />
-            <Route path="mutualfund" element={<MutualFundPage />} />
-            <Route path="gst" element={<GSTPage />} />
-            <Route path="updates" element={<UpdatesPage />} />
-            <Route path="blogs/new" element={<NewBlog />} />
-            <Route path="blogs/:id" element={<BlogDetail />} />
-            <Route path="*" element={<Navigate to="/mutualfund" replace />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </HelmetProvider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Navigate to="/mutualfund" replace />} />
+        <Route path="mutualfund" element={<MutualFundPage />} />
+        <Route path="gst" element={<GSTPage />} />
+        <Route path="updates" element={<UpdatesPage />} />
+        <Route path="blogs/new" element={<NewBlog />} /> {/* NEW */}
+        <Route path="blogs/:id" element={<BlogDetail />} /> {/* NEW */}
+        <Route path="*" element={<Navigate to="/mutualfund" replace />} />
+      </Route>
+    </Routes>
   );
 }
 
