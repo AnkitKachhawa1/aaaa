d6112738fe5d37417435635bf59a75f01c42eca8
AnkitKachhawa1 <117518485+AnkitKachhawa1@users.noreply.github.com>
2025-06-09 14:52:49 +0530

    Update index.ts

--- src/data/blogs/index.ts
+++ src/data/blogs/index.ts
@@ -1,7 +1,17 @@
 import financialPlanning from './financial-planning';
 import gstUpdates2025 from './gst-updates-2025';
 
+// Make sure all blog objects have tag and date fields!
 export const blogs = [
-  financialPlanning,
-  gstUpdates2025
+  {
+    ...financialPlanning,
+    tag: "mutual fund",
+    date: "2025-05-10",
+  },
+  {
+    ...gstUpdates2025,
+    tag: "gst",
+    date: "2025-06-01",
+  },
+  // Add more blogs as needed
 ];
