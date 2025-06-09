9640f917caf7934986a1d354bc6dd0bf85da0563
AnkitKachhawa1 <117518485+AnkitKachhawa1@users.noreply.github.com>
2025-06-09 18:19:22 +0530

    Create gst-document-list.ts

---
+++ src/data/blogs/gst-document-list.ts
@@ -0,0 +1,8 @@
+const gstDocumentList = {
+  link: "gst-document-list",
+  title: "DOCUMENT REQUIREMENT FOR GST REGISTRATIO",
+  date: "9 JUNE 2025",
+  tag: "GST",
+  content: "" // Add your content here later
+};
+export default gstDocumentList;
